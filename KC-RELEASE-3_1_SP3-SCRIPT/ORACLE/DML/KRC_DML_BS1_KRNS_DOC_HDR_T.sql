858b37ffd02ac175602de17d1f22c14843ade6cb
srawlins <sam.rawlins@gmail.com>
2011-08-22 07:20:13 -0700

    Hard-coded DOC_HDR_ID ...

--- KC-RELEASE-3_1_SP3-SCRIPT/ORACLE/DML/KRC_DML_BS1_KRNS_DOC_HDR_T.sql
+++ KC-RELEASE-3_1_SP3-SCRIPT/ORACLE/DML/KRC_DML_BS1_KRNS_DOC_HDR_T.sql
@@ -3,6 +3,6 @@
 SET DEFINE OFF;
 
 INSERT INTO KRNS_DOC_HDR_T (DOC_HDR_ID,VER_NBR,FDOC_DESC,ORG_DOC_HDR_ID,TMPL_DOC_HDR_ID,EXPLANATION,OBJ_ID) 
-  VALUES (10179,6,'Proposal Person Preloaded Questionnaire',null,null,null,SYS_GUID());
+  VALUES (KREW_DOC_HDR_S.nextVal,6,'Proposal Person Preloaded Questionnaire',null,null,null,SYS_GUID());
 
 COMMIT;
